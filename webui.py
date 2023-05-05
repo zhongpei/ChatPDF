f10a067617ead6879513a5eb3ae2a8fdf7b34789
fofo <qqqeee@gmail.com>
2023-05-05 09:17:35 +0800

    add lora load

--- webui.py
+++ webui.py
@@ -62,7 +62,6 @@ def upload_file(file):
     return gr.Dropdown.update(choices=file_list, value=filename)
 
 
-
 def parse_text(text):
     """copy from https://github.com/GaiZhenbiao/ChuanhuChatGPT/"""
     lines = text.split("\n")
@@ -125,11 +124,14 @@ def update_status(history, status):
     return history
 
 
-def reinit_model(llm_model, embedding_model, history):
+def reinit_model(llm_model, llm_lora, embedding_model, history):
     try:
         global model
         if model is not None:
             del model
+        llm_lora_path = None
+        if llm_lora is not None and os.path.exists(llm_lora):
+            llm_lora_path = llm_lora
         model = ChatPDF(
             sim_model_name_or_path=embedding_model_dict.get(
                 embedding_model,
@@ -137,7 +139,7 @@ def reinit_model(llm_model, embedding_model, history):
             ),
             gen_model_type=llm_model.split('-')[0],
             gen_model_name_or_path=llm_model_dict.get(llm_model, "THUDM/chatglm-6b-int4"),
-            lora_model_name_or_path=None,
+            lora_model_name_or_path=llm_lora_path
         )
 
         model_status = """模型已成功重新加载，请选择文件后点击"加载文件"按钮"""
@@ -227,6 +229,7 @@ with gr.Blocks(css=block_css) as demo:
                                  label="LLM 模型",
                                  value=list(llm_model_dict.keys())[0],
                                  interactive=True)
+            llm_lora = gr.Textbox(label="lora path",value="E:\\output")
             embedding_model = gr.Radio(embedding_model_dict_list,
                                        label="Embedding 模型",
                                        value=embedding_model_dict_list[0],
@@ -260,7 +263,7 @@ with gr.Blocks(css=block_css) as demo:
     load_model_button.click(
         reinit_model,
         show_progress=True,
-        inputs=[llm_model, embedding_model, chatbot],
+        inputs=[llm_model, llm_lora, embedding_model, chatbot],
         outputs=chatbot
     )
     # 将上传的文件保存到content文件夹下,并更新下拉框
